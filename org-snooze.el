8fa6e147a7ee74a70d2701882278b0b725e78e86
Bill Xue <xueeinstein@gmail.com>
2018-12-23 21:09:30 +0800

    implement `org-snooze-pop'
    Open most recently alerted item in one command.

--- org-snooze.el
+++ org-snooze.el
@@ -16,6 +16,7 @@
 
 ;;; Code:
 (require 'org-agenda)
+(require 'cl-lib)
 
 (defgroup org-snooze nil
   "Extension to snooze your code, doc and feed."
@@ -31,11 +32,9 @@
 (defvar org-snooze-records-file-title "Snoozed"
   "The title of org file which stores the snoozed items.")
 
-(defvar org-snooze-default-late-today "18:00"
-  "The default time of late today.")
-
-(defvar org-snooze-default-early-tomorrow "08:00"
-  "The default time of early tomorrow.")
+;; ===================================
+;; Implementation for `org-snooze'
+;; ===================================
 
 (defun org-snooze-strip-text-properties (txt)
   "Strip text TXT properties."
@@ -133,6 +132,100 @@ State characters are: TODO, DONE, NEXT, HOLD, WAITING."
 
     ;; notify success
     (message "Snoozed %s to %s" search time)))
+
+;; ===================================
+;; Implementation for `org-snooze-pop'
+;; ===================================
+
+(defun org-snooze--parse-record-file-ast ()
+  "Parse the abstract syntax tree (AST) of the file `org-snooze-records-file'."
+  (with-temp-buffer
+    (insert-file-contents org-snooze-records-file)
+    (org-mode)
+    (org-element-parse-buffer)))
+
+(defun org-snooze--parse-time-string (time-string)
+  "Parse TIME-STRING to comparable float time."
+  (let ((time (date-to-time time-string)))
+    (float-time time)))
+
+(defun org-snooze--parse-link (txt)
+  "Parse link for TXT."
+  (save-match-data
+    (if (string-match "\\[[a-zA-Z].*\\]\\[link\\]" txt)
+        (cl-subseq (match-string 0 txt) 1 -7))))
+
+(defun org-snooze--get-pop-item-plist (ast)
+  "Get information of pop item as plist through parsing AST.
+Return a plist in form (:pos HEADLINE-POS :link ORG-LINK :time ALERT-TIME)."
+  (let ((time-now (org-snooze--parse-time-string (current-time-string)))
+        (most-recent-time 0.0)
+        (headlines (cddr ast))
+        (headline-id 0)
+        (most-recent-id -1)
+        (most-recent-item nil)
+        (pos nil)
+        (link nil)
+        (alert-at nil))
+    ;; loop to find correct `most-recent-time' and `most-recent-id'
+    (dolist (h headlines)
+      (when (string-equal (plist-get (cadr h) :todo-keyword) "TODO")
+        (let* ((deadline (plist-get (cadr h) :deadline))
+               (timestamp (plist-get deadline 'timestamp))
+               (time-string (plist-get timestamp :raw-value))
+               (time-float (org-snooze--parse-time-string time-string)))
+          (if (and (< most-recent-time time-float)
+                   (< time-float time-now))
+              (progn
+                (setq most-recent-time time-float)
+                (setq most-recent-id headline-id)))))
+      (setq headline-id (+ headline-id 1)))
+
+    ;; get return values
+    (when (>= most-recent-id 0)
+      (setq most-recent-item (plist-get (nth most-recent-id headlines) 'headline))
+      (setq pos (plist-get most-recent-item :begin))
+      (setq link (org-snooze--parse-link
+                  (plist-get most-recent-item :raw-value)))
+      (setq alert-at (format-time-string
+                      "%Y-%m-%d %T" (seconds-to-time most-recent-time))))
+    (list :pos pos :link link :time alert-at)))
+
+(defun org-snooze--disabled-org-add-log-setup (&optional a b c d e)
+  "To disable `org-add-log-setup' with five args: A, B, C, D, E.
+These five args respectively correspond to `org-add-log-setup'
+five optional args."
+  nil)
+
+(defun org-snooze--mark-done (pos)
+  "Mark org TODO headline at POS as DONE."
+  (with-temp-buffer
+    (when (file-exists-p org-snooze-records-file)
+      (insert-file-contents org-snooze-records-file))
+    (org-mode)
+    (goto-char pos)
+    (cl-letf (((symbol-function 'org-add-log-setup)
+               #'org-snooze--disabled-org-add-log-setup))
+      ;; disable executation of `org-add-log-note'
+      (org-todo "DONE"))
+
+    (write-region nil nil org-snooze-records-file)))
+
+;;;###autoload
+(defun org-snooze-pop ()
+  "Main function to pop a snoozed item that Emacs agenda just send notification."
+  (interactive)
+  (let* ((ast (caddr (org-snooze--parse-record-file-ast)))
+         (item-plist (org-snooze--get-pop-item-plist ast))
+         (link (plist-get item-plist :link))
+         (pos (plist-get item-plist :pos))
+         (time (plist-get item-plist :time)))
+    (if (not link)
+        (message "Cannot find recently alerted snoozed item.")
+      (progn
+        (org-open-link-from-string link)
+        (org-snooze--mark-done pos)
+        (message "Pop snoozed item that alerted at %s" time)))))
 
 (provide 'org-snooze)
 ;;; org-snooze.el ends here
